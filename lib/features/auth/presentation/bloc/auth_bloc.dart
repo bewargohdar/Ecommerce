1b6cdea687ba176112d0ded918e825586d92f86c
bewar.gohdar <bewar.00429239@gmail.com>
2024-10-09 14:54:06 +0300

    Refactor AuthBloc to use separate methods for event handling

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -7,39 +7,41 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(ButtonInitialState()) {
-    // Handle FetchAges event
-    on<FetchAges>((event, emit) async {
-      emit(AgesLoading());
-      var returnedData = await sl<GetAgesUseCase>().call(null);
+    on<FetchAges>(_onFetchAges);
+    on<SelectAge>(_selectAge);
+    on<SelectGender>(_selectGender);
+    on<ExecuteUseCase>(_executeUseCase);
+  }
+  Future<void> _onFetchAges(FetchAges event, Emitter<AuthState> emit) async {
+    emit(AgesLoading());
+    var returnedData = await sl<GetAgesUseCase>().call(null);
 
+    returnedData.fold(
+      (message) => emit(AgesLoadFailure(message: message)),
+      (data) => emit(AgesLoaded(ages: data)),
+    );
+  }
+
+  Future<void> _selectAge(SelectAge event, Emitter<AuthState> emit) async {
+    emit(AgeSelectionState(event.age));
+  }
+
+  Future<void> _selectGender(
+      SelectGender event, Emitter<AuthState> emit) async {
+    emit(GenderSelectionState(event.index));
+  }
+
+  Future<void> _executeUseCase(
+      ExecuteUseCase event, Emitter<AuthState> emit) async {
+    emit(ButtonLoadingState());
+    try {
+      Either returnedData = await event.usecase.call(event.params);
       returnedData.fold(
-        (message) => emit(AgesLoadFailure(message: message)),
-        (data) => emit(AgesLoaded(ages: data)),
+        (error) => emit(ButtonFailureState(errorMessage: error)),
+        (data) => emit(ButtonSuccessState()),
       );
-    });
-
-    // Handle SelectAge event
-    on<SelectAge>((event, emit) {
-      emit(AgeSelectionState(event.age));
-    });
-
-    // Handle SelectGender event
-    on<SelectGender>((event, emit) {
-      emit(GenderSelectionState(event.index));
-    });
-
-    // Handle ExecuteUseCase event for button actions
-    on<ExecuteUseCase>((event, emit) async {
-      emit(ButtonLoadingState());
-      try {
-        Either returnedData = await event.usecase.call(event.params);
-        returnedData.fold(
-          (error) => emit(ButtonFailureState(errorMessage: error)),
-          (data) => emit(ButtonSuccessState()),
-        );
-      } catch (e) {
-        emit(ButtonFailureState(errorMessage: e.toString()));
-      }
-    });
+    } catch (e) {
+      emit(ButtonFailureState(errorMessage: e.toString()));
+    }
   }
 }

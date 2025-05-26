bb885a9cde9a57c2c7b5337ab02419bf9993ea57
bewar.gohdar <bewar.00429239@gmail.com>
2025-05-26 20:40:08 +0300

    feat(home): convert HomePage to StatefulWidget and fetch user info on init

--- lib/features/home/presentation/page/home.dart
+++ lib/features/home/presentation/page/home.dart
@@ -1,8 +1,22 @@
+import 'package:ecomerce/features/home/presentation/bloc/home_bloc.dart';
 import 'package:ecomerce/features/home/presentation/widget/header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    // Fetch user info only when home page is loaded
+    context.read<HomeBloc>().add(FetchUserInfo());
+  }
 
   @override
   Widget build(BuildContext context) {

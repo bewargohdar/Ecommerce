de816b202158e947b024b18ffcde4286fd791829
bewar.gohdar <bewar.00429239@gmail.com>
2025-05-27 14:41:12 +0300

    refactor(header): consolidate user profile and gender display into the Header widget

--- lib/features/home/presentation/widget/header.dart
+++ lib/features/home/presentation/widget/header.dart
@@ -1,7 +1,6 @@
 import 'package:ecomerce/common/helper/navigator/app_navigator.dart';
 import 'package:ecomerce/core/config/assets/app_images.dart';
 import 'package:ecomerce/core/config/theme/app_color.dart';
-import 'package:ecomerce/features/auth/domain/entity/user_entity.dart';
 import 'package:ecomerce/features/home/presentation/bloc/home_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,15 +16,61 @@ class Header extends StatelessWidget {
     return BlocBuilder<HomeBloc, HomeState>(
       builder: (context, state) {
         if (state is HomeLoading) {
-          return const Center(child: CircularProgressIndicator());
+          return SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: const Center(child: CircularProgressIndicator()),
+          );
         }
         if (state is HomeLoaded) {
           return Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              _profileImage(state.user, context),
-              _gender(state.user),
-              _card(context)
+              GestureDetector(
+                onTap: () {
+                  AppNavigator.push(context, const Scaffold());
+                },
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: state.user.image.isEmpty
+                              ? const AssetImage(AppImages.profile)
+                              : NetworkImage(state.user.image)),
+                      color: Colors.red,
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Container(
+                height: 40,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                    color: AppColors.secondBackground,
+                    borderRadius: BorderRadius.circular(100)),
+                child: Center(
+                  child: Text(
+                    state.user.gender == 1 ? 'Men' : 'Women',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w500, fontSize: 16),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  AppNavigator.push(context, const Scaffold());
+                },
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                      color: AppColors.primary, shape: BoxShape.circle),
+                  child: SvgPicture.asset(
+                    AppVectors.bag,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              )
             ],
           );
         }
@@ -39,59 +84,6 @@ class Header extends StatelessWidget {
         }
         return const SizedBox();
       },
-    );
-  }
-
-  Widget _profileImage(UserEntity user, BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        AppNavigator.push(context, const Scaffold());
-      },
-      child: Container(
-        height: 40,
-        width: 40,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: user.image.isEmpty
-                    ? const AssetImage(AppImages.profile)
-                    : NetworkImage(user.image)),
-            color: Colors.red,
-            shape: BoxShape.circle),
-      ),
-    );
-  }
-
-  Widget _gender(UserEntity user) {
-    return Container(
-      height: 40,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-          color: AppColors.secondBackground,
-          borderRadius: BorderRadius.circular(100)),
-      child: Center(
-        child: Text(
-          user.gender == 1 ? 'Men' : 'Women',
-          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-        ),
-      ),
-    );
-  }
-
-  Widget _card(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        AppNavigator.push(context, const Scaffold());
-      },
-      child: Container(
-        height: 40,
-        width: 40,
-        decoration: const BoxDecoration(
-            color: AppColors.primary, shape: BoxShape.circle),
-        child: SvgPicture.asset(
-          AppVectors.bag,
-          fit: BoxFit.none,
-        ),
-      ),
     );
   }
 }

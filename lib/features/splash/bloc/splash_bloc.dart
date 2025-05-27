a4e57bf90e69d1b14607e26587dbebb5f6ada0dd
bewar.gohdar <bewar.00429239@gmail.com>
2025-05-27 18:16:19 +0300

    refactor(splash_bloc): improve async handling and emit states based on isClosed check

--- lib/features/splash/bloc/splash_bloc.dart
+++ lib/features/splash/bloc/splash_bloc.dart
@@ -7,16 +7,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class SplashBloc extends Bloc<SplashEvent, SplashState> {
   SplashBloc() : super(DisplaySplash()) {
     on<AppStarted>((event, emit) async {
+      // Use Future.delayed instead of Timer for better async handling in tests
       await Future.delayed(const Duration(milliseconds: 2500));
-      try {
-        final isLogged = await sl<IsLoggedInUseCase>().call();
-        if (isLogged) {
-          emit(Authenticated());
-        } else {
-          emit(UnAuthenticated());
+
+      if (!isClosed) {
+        try {
+          final isLogged = await sl<IsLoggedInUseCase>().call();
+          if (isLogged && !isClosed) {
+            emit(Authenticated());
+          } else if (!isClosed) {
+            emit(UnAuthenticated());
+          }
+        } catch (e) {
+          if (!isClosed) {
+            emit(UnAuthenticated());
+          }
         }
-      } catch (e) {
-        emit(UnAuthenticated());
       }
     });
   }

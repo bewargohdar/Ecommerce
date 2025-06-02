1ac1bb31a5841a6db531daee5ad77d511bbf4c30
bewar.gohdar <bewar.00429239@gmail.com>
2025-06-02 03:13:38 +0300

    feat(product): add JSON serialization annotations to ProductEntity and related classes

--- lib/features/product/domain/entity/product.dart
+++ lib/features/product/domain/entity/product.dart
@@ -1,4 +1,8 @@
 // --- Entity Definitions (from your input) ---
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
 class DimensionsEntity {
   final double width;
   final double height;
@@ -42,27 +46,49 @@ class MetaEntity {
 }
 
 class ProductEntity {
+  @JsonKey(name: 'id')
   final int id;
+  @JsonKey(name: 'title')
   final String title;
+  @JsonKey(name: 'description')
   final String description;
+  @JsonKey(name: 'category')
   final String category;
+  @JsonKey(name: 'price')
   final double price;
+  @JsonKey(name: 'discountPercentage')
   final double discountPercentage;
+  @JsonKey(name: 'rating')
   final double rating;
+  @JsonKey(name: 'stock')
   final int stock;
+  @JsonKey(name: 'tags')
   final List<String> tags;
+  @JsonKey(name: 'brand')
   final String brand;
+  @JsonKey(name: 'sku')
   final String sku;
+  @JsonKey(name: 'weight')
   final int weight;
+  @JsonKey(name: 'dimensions')
   final DimensionsEntity dimensions;
+  @JsonKey(name: 'warrantyInformation')
   final String warrantyInformation;
+  @JsonKey(name: 'shippingInformation')
   final String shippingInformation;
+  @JsonKey(name: 'availabilityStatus')
   final String availabilityStatus;
+  @JsonKey(name: 'reviews')
   final List<ReviewEntity> reviews;
+  @JsonKey(name: 'returnPolicy')
   final String returnPolicy;
+  @JsonKey(name: 'minimumOrderQuantity')
   final int minimumOrderQuantity;
+  @JsonKey(name: 'meta')
   final MetaEntity meta;
+  @JsonKey(name: 'images')
   final List<String> images;
+  @JsonKey(name: 'thumbnail')
   final String thumbnail;
 
   const ProductEntity({

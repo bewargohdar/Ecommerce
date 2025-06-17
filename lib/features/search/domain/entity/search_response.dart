5dd67dcf0b6a65f127710faae941bce279c347fc
bewar.gohdar <bewar.00429239@gmail.com>
2025-06-18 00:17:27 +0300

    feat(search): add GeneralSearchResponse class to handle search results with optional category list

---
+++ lib/features/search/domain/entity/search_response.dart
@@ -0,0 +1,7 @@
+import 'package:ecomerce/features/category/domain/entity/category.dart';
+
+class GeneralSearchResponse {
+  final List<CategoryEntity>? categories;
+
+  GeneralSearchResponse({this.categories});
+}

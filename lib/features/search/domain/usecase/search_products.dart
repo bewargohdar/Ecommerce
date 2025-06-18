2132615b4319fe25c1232258f47a70abc0091a08
bewar.gohdar <bewar.00429239@gmail.com>
2025-06-18 21:42:04 +0300

    refactor(search): modify SearchProductsUseCase to return GeneralSearchResponseEntity instead of List<ProductEntity>

--- lib/features/search/domain/usecase/search_products.dart
+++ lib/features/search/domain/usecase/search_products.dart
@@ -1,16 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecomerce/core/usecase/usecase.dart';
-import 'package:ecomerce/features/product/domain/entity/product.dart';
+import 'package:ecomerce/features/search/domain/entity/search_response.dart';
 import 'package:ecomerce/features/search/domain/repository/search_repo.dart';
 
 class SearchProductsUseCase
-    extends Usecase<Either<String, List<ProductEntity>>, String> {
+    extends Usecase<Either<String, GeneralSearchResponseEntity>, String> {
   final SearchRepository searchRepository;
 
   SearchProductsUseCase(this.searchRepository);
 
   @override
-  Future<Either<String, List<ProductEntity>>> call({String? params}) {
+  Future<Either<String, GeneralSearchResponseEntity>> call({String? params}) {
     return searchRepository.searchProducts(params!);
   }
 }

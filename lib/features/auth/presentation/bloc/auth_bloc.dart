9c6a59fe68270785b39d6d419a27466167c31086
bewar.gohdar <bewar.00429239@gmail.com>
2025-06-21 18:28:29 +0300

    feat(auth): refactor AuthBloc to use dependency injection for use cases

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,28 @@
 import 'package:dartz/dartz.dart';
 import 'package:ecomerce/features/auth/domain/usecase/get_ages.dart';
+import 'package:ecomerce/features/auth/domain/usecase/send_password_reset.dart';
 import 'package:ecomerce/features/auth/domain/usecase/signin.dart';
+import 'package:ecomerce/features/auth/domain/usecase/signup.dart';
 import 'package:ecomerce/features/auth/presentation/bloc/auth_event.dart';
 import 'package:ecomerce/features/auth/presentation/bloc/auth_state.dart';
-import 'package:ecomerce/service_locator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(ButtonInitialState()) {
+  final GetAgesUseCase _getAgesUseCase;
+  final SigninUsecase _signinUsecase;
+  final SignupUsecase _signupUsecase;
+  final SendPasswordResetEmailUseCase _sendPasswordResetEmailUseCase;
+
+  AuthBloc({
+    required GetAgesUseCase getAgesUseCase,
+    required SigninUsecase signinUsecase,
+    required SignupUsecase signupUsecase,
+    required SendPasswordResetEmailUseCase sendPasswordResetEmailUseCase,
+  })  : _getAgesUseCase = getAgesUseCase,
+        _signinUsecase = signinUsecase,
+        _signupUsecase = signupUsecase,
+        _sendPasswordResetEmailUseCase = sendPasswordResetEmailUseCase,
+        super(ButtonInitialState()) {
     on<FetchAges>(_onFetchAges);
     on<SelectAge>(_selectAge);
     on<SelectGender>(_selectGender);
@@ -16,7 +31,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   }
   Future<void> _onFetchAges(FetchAges event, Emitter<AuthState> emit) async {
     emit(AgesLoading());
-    var returnedData = await sl<GetAgesUseCase>().call();
+    var returnedData = await _getAgesUseCase.call(event);
 
     returnedData.fold(
       (message) => emit(AgesLoadFailure(message: message)),
@@ -37,7 +52,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       ExecuteUseCase event, Emitter<AuthState> emit) async {
     emit(ButtonLoadingState());
     try {
-      Either returnedData = await event.usecase.call(params: event.params);
+      Either returnedData = await event.usecase.call(event.params);
       returnedData.fold(
         (error) => emit(ButtonFailureState(errorMessage: error)),
         (data) => emit(ButtonSuccessState()),
@@ -50,8 +65,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   Future<void> _signIn(SignInEvent event, Emitter<AuthState> emit) async {
     emit(ButtonLoadingState());
     try {
-      Either returnedData =
-          await sl<SigninUsecase>().call(params: event.signinUserReq);
+      Either returnedData = await _signinUsecase.call(event.signinUserReq);
       returnedData.fold(
         (error) => emit(ButtonFailureState(errorMessage: error)),
         (data) => emit(ButtonSuccessState()),

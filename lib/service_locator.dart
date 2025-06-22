b537c06fbe9e14ec5d31f2bb1d88ed5a47fde1d6
bewar.gohdar <bewar.00429239@gmail.com>
2025-06-22 19:37:08 +0300

    feat(service_locator): add splash and home dependencies to service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,6 +4,9 @@ import 'package:ecomerce/features/auth/auth_injection.dart';
 import 'package:ecomerce/features/category/category_injection.dart';
 import 'package:ecomerce/features/product/product_injection.dart';
 import 'package:ecomerce/features/search/search_injection.dart';
+import 'package:ecomerce/features/splash/splash_injection.dart';
+
+import '../../features/home/home_injection.dart';
 
 final sl = GetIt.instance;
 
@@ -16,4 +19,6 @@ Future<void> initializeDependencies() async {
   setUpCategoryDependencies();
   setUpProductDependencies();
   setUpSearchDependencies();
+  setUpSplashDependencies();
+  setUpHomeDependencies();
 }

c39c8cedcc47fc13b90f2f9d340a391c69e47b28
bewar.gohdar <bewar.00429239@gmail.com>
2025-06-22 20:19:56 +0300

    feat(product_carousel): add ProductCarousel widget for displaying products in a horizontal list

---
+++ lib/common/widget/product/product_carousel.dart
@@ -0,0 +1,30 @@
+import 'package:ecomerce/common/widget/product/product_cart.dart';
+import 'package:ecomerce/features/product/domain/entity/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductCarousel extends StatelessWidget {
+  final List<ProductEntity> products;
+  const ProductCarousel({
+    super.key,
+    required this.products,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: ListView.separated(
+          separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+          itemCount: products.length,
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          itemBuilder: (context, index) {
+            return ProductCart(
+              product: products[index],
+            );
+          }),
+    );
+  }
+}

bd9181616626f4a24074f9358d3d0892b5cdcb91
bewar.gohdar <bewar.00429239@gmail.com>
2025-06-29 18:02:17 +0300

    feat(colors): add white color constant to AppColors for enhanced theming options

--- lib/core/config/theme/app_color.dart
+++ lib/core/config/theme/app_color.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const primary = Color(0xff8E6CEF);
   static const background = Color(0xff1D182A);
   static const secondBackground = Color(0xff342F3F);
+  static const white = Color(0xffffffff);
 }

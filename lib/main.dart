24cb78ec072945c489fbf8143596b607906f30a0
bewar.gohdar <bewar.00429239@gmail.com>
2025-07-04 02:59:21 +0300

    feat(router): implement AppRouter for dynamic home widget based on authentication state

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:ecomerce/core/config/theme/app_theme.dart';
+import 'package:ecomerce/core/usecase/usecase.dart';
+import 'package:ecomerce/features/auth/domain/usecase/is_logged_in.dart';
 import 'package:ecomerce/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:ecomerce/features/auth/presentation/page/signin_page.dart';
 import 'package:ecomerce/features/category/presentation/bloc/category_bloc.dart';
 import 'package:ecomerce/features/home/presentation/bloc/home_bloc.dart';
 import 'package:ecomerce/features/product/presentation/bloc/product_bloc.dart';
@@ -7,6 +10,7 @@ import 'package:ecomerce/features/search/presentation/bloc/search_bloc.dart';
 import 'package:ecomerce/features/splash/bloc/splash_bloc.dart';
 import 'package:ecomerce/features/splash/bloc/splash_event.dart';
 import 'package:ecomerce/features/splash/page/splash.dart';
+import 'package:ecomerce/features/tabs/presentation/page/tabs_page.dart';
 import 'package:ecomerce/firebase_options.dart';
 import 'package:ecomerce/service_locator.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -55,6 +59,50 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Ecomerce',
             theme: AppTheme.appTheme,
-            home: const Splash()),
+            home: const AppRouter()),
       );
+}
+
+class AppRouter extends StatefulWidget {
+  const AppRouter({super.key});
+
+  @override
+  State<AppRouter> createState() => _AppRouterState();
+}
+
+class _AppRouterState extends State<AppRouter> {
+  bool _isInitialized = false;
+  Widget? _homeWidget;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeApp();
+  }
+
+  Future<void> _initializeApp() async {
+    try {
+      final isLoggedInUseCase = sl<IsLoggedInUseCase>();
+      final isLoggedIn = await isLoggedInUseCase.call(NoParams());
+
+      setState(() {
+        _homeWidget = isLoggedIn ? const TabsPage() : SigninPage();
+        _isInitialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _homeWidget = SigninPage();
+        _isInitialized = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isInitialized) {
+      return const Splash();
+    }
+
+    return _homeWidget!;
+  }
 }

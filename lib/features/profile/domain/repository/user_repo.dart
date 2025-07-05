ebe9a8a952175a1a0811cf6415cea953583917e6
bewar.gohdar <bewar.00429239@gmail.com>
2025-07-05 12:33:51 +0300

    feat(profile): remove UserRepo interface as it is no longer needed

--- lib/features/profile/domain/repository/user_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:ecomerce/core/resource/data_state.dart';
-import 'package:ecomerce/features/auth/domain/entity/user_entity.dart';
-
-abstract class UserRepo {
-  Future<DataState<UserEntity>> getSingleUser();
-}

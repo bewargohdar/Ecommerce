60a66928ed1a80f54a71e1674ab84cefd68729d9
bewar.gohdar <bewar.00429239@gmail.com>
2025-08-19 08:06:27 +0300

    fix(auth_repository): update return types to DataState for consistency in AuthRepository methods

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,12 +1,12 @@
-import 'package:dartz/dartz.dart';
+import 'package:ecomerce/core/resource/data_state.dart';
 import 'package:ecomerce/features/auth/data/models/signin_user_req.dart';
 import 'package:ecomerce/features/auth/data/models/signup_model.dart';
 
 abstract class AuthRepository {
-  Future<Either> signup(UserCredentialRequestModel userModel);
-  Future<Either> getAges();
-  Future<Either> signin(SigninUserReq userModel);
-  Future<Either> sendPasswordResetEmail(String email);
+  Future<DataState> signup(UserCredentialRequestModel userModel);
+  Future<DataState> getAges();
+  Future<DataState> signin(SigninUserReq userModel);
+  Future<DataState> sendPasswordResetEmail(String email);
   Future<bool> isLoggedIn();
-  Future<Either> getUser();
+  Future<DataState> getUser();
 }
